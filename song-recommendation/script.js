10ebe762e9640c8c8476a5b41eb4d445bf9fc596
develis <emarcelatfs@gmail.com>
2024-03-22 17:38:17 -0300

    [song-recommendation] Reorganizing

--- song-recommendation/script.js
+++ song-recommendation/script.js
@@ -1,12 +1,12 @@
+const currentDate = new Date();
+const formattedDate = currentDate.toLocaleDateString('pt-BR', { day: 'numeric', month: 'long', year: 'numeric' });
+const dateElement = document.querySelector('div p');
+
 window.onload = () => {
     const defaultSongUrl = "https://open.spotify.com/embed/track/4bLbEtNtOK3eYbmaMCLDUv?utm_source=generator&theme=0";
     const urlParams = new URLSearchParams(window.location.search);
     const songUrl = urlParams.get('songUrl') || defaultSongUrl;
+
     document.querySelector('iframe').src = songUrl;
+    dateElement.innerHTML = `${formattedDate},<br> te dedico essa música:`;
 };
-
-const currentDate = new Date();
-const formattedDate = currentDate.toLocaleDateString('pt-BR', { day: 'numeric', month: 'long', year: 'numeric' });
-
-const dateElement = document.querySelector('div p');
-dateElement.innerHTML = `${formattedDate},<br> te dedico essa música:`;
