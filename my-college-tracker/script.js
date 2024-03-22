c6d22921c9e5a42937c2452faf2609c72c3933df
develis <emarcelatfs@gmail.com>
2024-03-22 14:33:59 -0300

    [my-college-tracker] Code adjustments

--- my-college-tracker/script.js
+++ my-college-tracker/script.js
@@ -15,29 +15,15 @@ window.onload = function () {
 fetch('obs.json')
     .then(response => response.json())
     .then(data => {
-        var semestres = data.reduce((groups, item) => {
-            var semestre = item.semestre;
-            if (!groups[semestre]) {
-                groups[semestre] = [];
-            }
-            groups[semestre].push(item);
-            return groups;
-        }, {});
+        const semestres = groupBySemestre(data);
 
-        var container = document.querySelector('body');
+        const container = document.querySelector('body');
 
-        for (var semestre in semestres) {
-            var h3 = document.createElement('h3');
-            h3.textContent = 'Semestre ' + semestre;
+        for (const semestre in semestres) {
+            const h3 = createHeading('Semestre ' + semestre);
             container.appendChild(h3);
 
-            var table = document.createElement('table');
-            var tbody = document.createElement('tbody');
-            table.appendChild(tbody);
-
-            var rows = semestres[semestre].map(createRow).join('');
-            tbody.innerHTML = rows;
-
+            const table = createTable(semestres[semestre]);
             container.appendChild(table);
         }
 
@@ -45,82 +31,16 @@ fetch('obs.json')
         loadColors();
     });
 
-function createRow(item) {
-    return `
-        <tr>
-            <td>${item.codigo} - ${item.nome}</td>
-            <td>
-                <button class="completed">Completed</button>
-                <button class="current">Current</button>
-                <button class="considering">Considering</button>
-                <button class="not-taken">Not Taken</button>
-            </td>
-        </tr>
-    `;
-}
-
-function addEventListeners() {
-    const colorMap = {
-        'completed': 'green',
-        'current': 'blue',
-        'considering': 'orange',
-        'not-taken': 'grey'
-    };
-
-    document.querySelectorAll('button').forEach(button => {
-        button.addEventListener('click', event => {
-            const materiaCell = event.target.parentElement.previousElementSibling;
-            const color = colorMap[event.target.className];
-            materiaCell.style.color = color;
-            localStorage.setItem(materiaCell.textContent, color);
-        });
-    });
-
-}
-
 function addOptativa() {
     const optativaInput = document.getElementById("optativaInput");
     const optativasList = document.getElementById("optativasList");
-    const row = document.createElement("tr");
 
-    const cell1 = document.createElement("td");
-    cell1.textContent = optativaInput.value;
-    row.appendChild(cell1);
-
-    const cell2 = document.createElement("td");
-    const button = document.createElement("button");
-    button.textContent = "Remover";
-    button.addEventListener('click', function () {
-        const optativas = JSON.parse(localStorage.getItem("optativas"));
-        const index = optativas.indexOf(optativaInput.value);
-        if (index > -1) {
-            optativas.splice(index, 1);
-        }
-        localStorage.setItem("optativas", JSON.stringify(optativas));
-        row.parentNode.removeChild(row);
-    });
-    cell2.appendChild(button);
-    row.appendChild(cell2);
-
+    const row = createOptativaRow(optativaInput.value);
     optativasList.appendChild(row);
 
-    let optativas = JSON.parse(localStorage.getItem("optativas"));
-
-    if (!optativas) {
-        optativas = [];
-    }
-
-    optativas.push(optativaInput.value);
-    localStorage.setItem("optativas", JSON.stringify(optativas));
+    addOptativaToLocalStorage(optativaInput.value);
 
     optativaInput.value = "";
-}
-
-function updateHours(elementId, value) {
-    if (value) {
-        document.getElementById(elementId).textContent = value;
-        localStorage.setItem(elementId, value);
-    }
 }
 
 function loadOptativas() {
@@ -147,27 +67,19 @@ function loadOptativas() {
     }
 }
 
-function createRemoveButton(optativa, row) {
-    const button = document.createElement("button");
-    button.textContent = "Remover";
-    button.addEventListener('click', function() {
-        removeOptativaFromLocalStorage(optativa);
-        removeRowFromTable(row);
-    });
-    return button;
-}
-
-function removeOptativaFromLocalStorage(optativa) {
-    let optativas = JSON.parse(localStorage.getItem("optativas"));
-    const index = optativas.indexOf(optativa);
-    if (index > -1) {
-        optativas.splice(index, 1);
+function deleteAllOptativas() {
+    localStorage.removeItem('optativas');
+    const optativasList = document.getElementById('optativasList');
+    while (optativasList.rows.length > 1) {
+        optativasList.deleteRow(1);
     }
-    localStorage.setItem("optativas", JSON.stringify(optativas));
 }
 
-function removeRowFromTable(row) {
-    row.parentNode.removeChild(row);
+function updateHours(elementId, value) {
+    if (value) {
+        document.getElementById(elementId).textContent = value;
+        localStorage.setItem(elementId, value);
+    }
 }
 
 function loadColors() {
@@ -190,23 +102,133 @@ function clearCellsColor() {
     }
 }
 
-document.getElementById('deleteAllButton').addEventListener('click', function () {
-    document.getElementById('confirmModal').style.display = 'block';
-});
+function groupBySemestre(data) {
+    return data.reduce((groups, item) => {
+        const semestre = item.semestre;
+        if (!groups[semestre]) {
+            groups[semestre] = [];
+        }
+        groups[semestre].push(item);
+        return groups;
+    }, {});
+}
 
-document.getElementById('confirmButton').addEventListener('click', function () {
+function getColorFromClass(className) {
+    const colorMap = {
+        'completed': 'green',
+        'current': 'blue',
+        'considering': 'orange',
+        'not-taken': 'grey'
+    };
+    return colorMap[className];
+}
+
+function addOptativaToLocalStorage(optativa) {
+    let optativas = JSON.parse(localStorage.getItem("optativas")) || [];
+    optativas.push(optativa);
+    localStorage.setItem("optativas", JSON.stringify(optativas));
+}
+
+function removeOptativaFromLocalStorage(optativa) {
+    let optativas = JSON.parse(localStorage.getItem("optativas"));
+    const index = optativas.indexOf(optativa);
+    if (index > -1) {
+        optativas.splice(index, 1);
+    }
+    localStorage.setItem("optativas", JSON.stringify(optativas));
+}
+
+function addEventListeners() {
+    const colorMap = {
+        'completed': 'green',
+        'current': 'blue',
+        'considering': 'orange',
+        'not-taken': 'grey'
+    };
+
+    document.querySelectorAll('button').forEach(button => {
+        button.addEventListener('click', event => {
+            const materiaCell = event.target.parentElement.previousElementSibling;
+            const color = colorMap[event.target.className];
+            materiaCell.style.color = color;
+            localStorage.setItem(materiaCell.textContent, color);
+        });
+    });
+
+}
+
+function createRemoveButton(optativa, row) {
+    const button = document.createElement("button");
+    button.textContent = "Remover";
+    button.addEventListener('click', function () {
+        removeOptativaFromLocalStorage(optativa);
+        row.parentNode.removeChild(row);
+    });
+    return button;
+}
+
+function createButtonRow(item) {
+    return `
+        <tr>
+            <td>${item.codigo} - ${item.nome}</td>
+            <td>
+                <button class="completed">Completed</button>
+                <button class="current">Current</button>
+                <button class="considering">Considering</button>
+                <button class="not-taken">Not Taken</button>
+            </td>
+        </tr>
+    `;
+}
+
+function createOptativaRow(optativa) {
+    const row = document.createElement("tr");
+
+    const cell1 = document.createElement("td");
+    cell1.textContent = optativa;
+    row.appendChild(cell1);
+
+    const cell2 = document.createElement("td");
+    const button = createRemoveButton(optativa, row);
+    cell2.appendChild(button);
+    row.appendChild(cell2);
+
+    return row;
+}
+
+function createHeading(text) {
+    const h3 = document.createElement('h3');
+    h3.textContent = text;
+    return h3;
+}
+
+function createTable(items) {
+    const table = document.createElement('table');
+    const tbody = document.createElement('tbody');
+    table.appendChild(tbody);
+
+    const rows = items.map(createButtonRow).join('');
+    tbody.innerHTML = rows;
+
+    return table;
+}
+
+function handleButtonClick(event) {
+    const materiaCell = event.target.parentElement.previousElementSibling;
+    const color = getColorFromClass(event.target.className);
+    materiaCell.style.color = color;
+    localStorage.setItem(materiaCell.textContent, color);
+}
+
+function handleButtonConfirmModal() {
     deleteAllOptativas();
     document.getElementById('confirmModal').style.display = 'none';
-});
+}
 
-document.getElementById('cancelButton').addEventListener('click', function () {
+function handleButtonCancelModal() {
     document.getElementById('confirmModal').style.display = 'none';
-});
+}
 
-function deleteAllOptativas() {
-    localStorage.removeItem('optativas');
-    const optativasList = document.getElementById('optativasList');
-    while (optativasList.rows.length > 1) {
-        optativasList.deleteRow(1);
-    }
+function handleButtonDeleteAllOptativas() {
+    document.getElementById('confirmModal').style.display = 'block';
 }
